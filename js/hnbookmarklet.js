712d6e86967d951cae88c9102268558a1b093732
Dylan Roy <dylan.k.roy@gmail.com>
2013-12-06 18:17:43 -0600

    Update hnbookmarklet.js

--- js/hnbookmarklet.js
+++ js/hnbookmarklet.js
@@ -144,7 +144,7 @@ var hnbookmarklet = (function( global, undefined ) {
 		attachStyle(location.protocol.replace('file','http')+'//rawgithub.com/dkroy/hnbookmarklet/gh-pages/css/hnbookmarklet.css');
 		var html = '<div id="hnbookmarklet-container"> \
 			<div id="hnbookmarklet-header"> \
-				<span id="logo"><a id="frontpage-hn" href="#"><img src="https://news.ycombinator.com/y18.gif" /> HN Boomarklet</a></span> \
+				<span id="hnb-logo"><a id="frontpage-hn" href="#"><img src="https://news.ycombinator.com/y18.gif" /> HN Boomarklet</a></span> \
 				<a href="javascript:window.location=%22http://news.ycombinator.com/submitlink?u=%22+encodeURIComponent(document.location)+%22&t=%22+encodeURIComponent(document.title);">Submit</a> | \
 				<a id="newest" href="#">Newest</a> | \
 				<a id="show-hn" href="#">Show HN</a> | \
